db2886e394c2b348ddc7246fe9e5a2b310cc94a8
MINT <mint@insdim.com>
2020-03-04 12:03:56 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,7 @@ module.exports = {
   pwa: {
     name: 'DIMedge',
     themeColor: '#46B49E',
-    msTileColor: '#22967B',
-    manifestOptions: {
-      background_color: '#FFFFFF'
-    }
-  }
-}
+    msTileColor: '#22967B'
+  },
+  transpileDependencies: ['vuetify']
+};
