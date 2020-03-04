03d047e04c05e53b56931247e3c192fb8a52db28
MINT <mint@insdim.com>
2020-03-04 12:01:56 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'DIMedge',
+    themeColor: '#46B49E',
+    msTileColor: '#22967B',
+    manifestOptions: {
+      background_color: '#FFFFFF'
+    }
+  }
+}
